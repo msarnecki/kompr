5b927745063a064b486a4aa0c587af18351a9568
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-09 23:48:59 +0100

    Added Compressor Service

---
+++ src/main/java/pl/pw/edu/ee/kompresja/compress/CompressorService.java
@@ -0,0 +1,12 @@
+package pl.pw.edu.ee.kompresja.compress;
+
+/**
+ * @author Marcin Jasion <marcin.jasion@gmail.com>
+ */
+public interface CompressorService {
+
+    public void compressWithLZMA();
+
+    public void compressWithLZW();
+
+}
---
+++ src/main/java/pl/pw/edu/ee/kompresja/compress/CompressorServiceImpl.java
@@ -0,0 +1,19 @@
+package pl.pw.edu.ee.kompresja.compress;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcin Jasion <marcin.jasion@gmail.com>
+ */
+@Component("compressor")
+public class CompressorServiceImpl implements CompressorService {
+    @Override
+    public void compressWithLZMA() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void compressWithLZW() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
