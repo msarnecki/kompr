2f101740d8890b1d227dcacfdb0ad11138f944b1
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-09 23:58:34 +0100

    added lzwService

---
+++ src/main/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceImpl.java
@@ -0,0 +1,15 @@
+package pl.pw.edu.ee.kompresja.service.compress.lzw;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcin Jasion <marcin.jasion@gmail.com>
+ */
+
+@Component("lzwService")
+public class LZWServiceImpl implements LZWService {
+    @Override
+    public void compress() {
+
+    }
+}
