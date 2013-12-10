acb29e735336056e1b37e1b466b1c75724b834d8
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-10 15:44:23 +0100

    added  logger ;)

--- src/main/java/pl/pw/edu/ee/kompresja/controller/BaseKomprController.java
+++ src/main/java/pl/pw/edu/ee/kompresja/controller/BaseKomprController.java
@@ -1,5 +1,7 @@
 package pl.pw.edu.ee.kompresja.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class BaseKomprController {
+    private Logger log = LoggerFactory.getLogger(BaseKomprController.class);
 
     @RequestMapping("/")
     String home() {
+        log.info("asd");
         return "index";
     }
 }
--- src/main/java/pl/pw/edu/ee/kompresja/service/compressor/CompressorServiceImpl.java
+++ src/main/java/pl/pw/edu/ee/kompresja/service/compressor/CompressorServiceImpl.java
@@ -1,5 +1,7 @@
 package pl.pw.edu.ee.kompresja.service.compressor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import pl.pw.edu.ee.kompresja.model.CompressInfoFile;
 
@@ -9,6 +11,8 @@ import pl.pw.edu.ee.kompresja.model.CompressInfoFile;
  */
 @Component("compressor")
 public class CompressorServiceImpl implements CompressorService {
+    private Logger log = LoggerFactory.getLogger(CompressorServiceImpl.class);
+
     @Override
     public CompressInfoFile compressWithLZMA() {
         //To change body of implemented methods use File | Settings | File Templates.
