3c3fb45b699323148507892a18fb95d6cdb91893
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-16 20:56:18 +0100

    implemented LZW compress algorithm

--- src/main/java/pl/pw/edu/ee/kompresja/service/compress/Compress.java
+++ src/main/java/pl/pw/edu/ee/kompresja/service/compress/Compress.java
@@ -1,10 +1,12 @@
 package pl.pw.edu.ee.kompresja.service.compress;
 
+import java.util.List;
+
 /**
  * @author Marcin Jasion <marcin.jasion@gmail.com>
  */
 public interface Compress {
 
-    void compress();
+    List<Integer> compress(String uncompressedText);
 
 }
--- src/main/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceImpl.java
+++ src/main/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceImpl.java
@@ -1,15 +1,82 @@
 package pl.pw.edu.ee.kompresja.service.compress.lzw;
 
-import org.springframework.stereotype.Component;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Marcin Jasion <marcin.jasion@gmail.com>
  */
 
-@Component("lzwService")
+@Service("lzwService")
 public class LZWServiceImpl implements LZWService {
-    @Override
-    public void compress() {
+    private Logger log = LoggerFactory.getLogger(LZWServiceImpl.class);
+    public static final int START_DICT_SIZE = 256;
 
+
+    @Override
+    public List<Integer> compress(String uncompressedText) {
+
+        Map<String, Integer> dictionary = Maps.newTreeMap();
+
+        for (int i = 0; i < START_DICT_SIZE; i++)
+            dictionary.put(Character.toString((char) i), i);
+
+        String w = "";
+        List<Integer> compressResult = Lists.newLinkedList();
+
+        for (char c : uncompressedText.toCharArray()) {
+
+            String wc = w + c;
+
+            if(dictionary.containsKey(wc))
+                w = wc;
+
+            else {
+                int newDictionaryKey = dictionary.keySet().size();
+                dictionary.put(wc, newDictionaryKey);
+                compressResult.add(dictionary.get(w));
+                w = Character.toString(c);
+            }
+        }
+
+        if (StringUtils.isNotEmpty(w))
+            compressResult.add(dictionary.get(w));
+
+        return compressResult;
+    }
+
+    private List<Integer> compress(String uncompressedText, Map<String, Integer> dictionary) {
+        for (int i = 0; i < START_DICT_SIZE; i++)
+            dictionary.put(Character.toString((char) i), i);
+
+        String w = "";
+        List<Integer> compressResult = Lists.newLinkedList();
+
+        for (char c : uncompressedText.toCharArray()) {
+
+            String wc = w + c;
+
+            if(dictionary.containsKey(wc))
+                w = wc;
+
+            else {
+                int newDictionaryKey = dictionary.keySet().size()+1;
+                dictionary.put(wc, newDictionaryKey);
+                compressResult.add(dictionary.get(w));
+                w = Character.toString(c);
+            }
+        }
+
+        if (StringUtils.isNotEmpty(w))
+            compressResult.add(dictionary.get(w));
+
+        return compressResult;
     }
 }
