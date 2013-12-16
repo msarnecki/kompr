8ecaaf19887c9f8a35e7a2cae11915dd53926cde
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-16 21:10:27 +0100

    improvements in tests: added data provider

--- src/test/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceImplTests.java
+++ src/test/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceImplTests.java
@@ -4,7 +4,7 @@ import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.pw.edu.ee.kompresja.KomprApplication;
 
@@ -22,30 +22,40 @@ import static org.testng.Assert.assertNotSame;
 @Test
 @ContextConfiguration(classes = KomprApplication.class)
 public class LZWServiceImplTests extends AbstractTestNGSpringContextTests {
-    private static String TO_BE_TEXT;
-    private static List<Integer> COMPRESSED_TEXT;
+    private static String TO_BE_TEXT = "TOBEORNOTTOBEORTOBEORNOT";
+    private static List<Integer> EXPECTED_COMPRESSED_TEXT = Lists.newArrayList(84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263);
 
     @Autowired
     LZWService lzwService;
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        TO_BE_TEXT = "TOBEORNOTTOBEORTOBEORNOT";
-        COMPRESSED_TEXT = Lists.newArrayList(84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263);
-
+    @DataProvider(name = "compress-text-provider")
+    public Object[][] parameterCompressTexts() {
+        return new Object[][] { { "ABRACADABRA!", Lists.newArrayList(65, 66, 82, 65, 67, 65, 68, 256, 258, 33) },
+                                { "Kompresja jest Spoko", Lists.newArrayList(75, 111, 109, 112, 114, 101, 115, 106, 97, 32, 106, 261, 116, 32, 83, 112, 111, 107, 111) } };
     }
+
 
     public void shouldCompressToBeText() throws Exception {
         List<Integer> compressResult = lzwService.compress(TO_BE_TEXT);
 
         assertNotNull(compressResult);
-        assertEquals(compressResult, COMPRESSED_TEXT);
+        assertEquals(compressResult, EXPECTED_COMPRESSED_TEXT);
     }
+
     public void shouldFailCompressToBeText() throws Exception {
-        List<Integer> compressResult = lzwService.compress(TO_BE_TEXT+"#");
+        List<Integer> compressResult = lzwService.compress(TO_BE_TEXT + "#");
 
         assertNotNull(compressResult);
-        assertNotEquals(compressResult, COMPRESSED_TEXT);
-        assertNotSame(compressResult, COMPRESSED_TEXT);
+        assertNotEquals(compressResult, EXPECTED_COMPRESSED_TEXT);
+        assertNotSame(compressResult, EXPECTED_COMPRESSED_TEXT);
     }
+
+    @Test(dataProvider = "compress-text-provider")
+    public void shouldCompressTextsFromDataProvider(String text, List<Integer> expectedResultList) {
+        List<Integer> compressResult = lzwService.compress(text);
+        System.out.println("compressResult = " + compressResult);
+        assertNotNull(compressResult);
+        assertEquals(compressResult, expectedResultList);
+    }
+
 }
