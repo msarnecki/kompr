3e82e3b907fe75c866ea242fc292368313e72e48
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-24 09:46:01 +0100

    added file upload handler

--- src/main/java/pl/pw/edu/ee/kompresja/KomprApplication.java
+++ src/main/java/pl/pw/edu/ee/kompresja/KomprApplication.java
@@ -2,8 +2,11 @@ package pl.pw.edu.ee.kompresja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @EnableAutoConfiguration
@@ -11,5 +14,10 @@ import org.springframework.context.annotation.Configuration;
 public class KomprApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(KomprApplication.class, args);
+    }
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        return new MultipartConfigElement("");
     }
 }
--- src/main/java/pl/pw/edu/ee/kompresja/controller/BaseKomprController.java
+++ src/main/java/pl/pw/edu/ee/kompresja/controller/BaseKomprController.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Marcin Jasion <marcin.jasion@gmail.com>
@@ -14,7 +17,14 @@ public class BaseKomprController {
 
     @RequestMapping("/")
     String home() {
-        log.info("asd");
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    String postFile(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        log.info("fileName = " + fileName);
+
         return "index";
     }
 }
