449bb0f7e7baab3fa3a9135ad84f0ed2f38d28d1
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-29 22:52:06 +0100

    added test for compressing file

--- src/test/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceFileTests.java
+++ src/test/java/pl/pw/edu/ee/kompresja/service/compress/lzw/LZWServiceFileTests.java
@@ -1,12 +1,17 @@
 package pl.pw.edu.ee.kompresja.service.compress.lzw;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.pw.edu.ee.kompresja.KomprApplication;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,9 +24,26 @@ public class LZWServiceFileTests extends AbstractTestNGSpringContextTests {
     @Autowired
     LZWService lzwService;
 
+    @Value("classpath:test_lzw.txt")
+    Resource testLzw;
+
+
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void shouldGetIllegalArgumentExceptionOnEmptyText() {
         List<Integer> compressResult = lzwService.compress(new File("im_not_exists.txt"));
     }
 
+    @Test
+    public void shouldCompresTestLZWFile() throws Exception {
+        // given
+        File testFile = testLzw.getFile();
+        ArrayList<Integer> expected = Lists.newArrayList(75, 111, 109, 112, 114, 101, 115, 106, 97, 32, 106, 261, 116, 32, 83, 112, 111, 107, 111);
+
+        // when
+        List<Integer> compressResult = lzwService.compress(testFile);
+
+        // then
+        Assert.assertNotNull(compressResult);
+        Assert.assertEquals(compressResult, expected);
+    }
 }
